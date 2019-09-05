07bfa0d520fcf6da0f8a5b93620260343f5c4e6c
vitek <vit.urbanec@gmail.com>
2019-09-05 22:42:49 +0300

    postgres integration WIP, added reqs

--- consume.py
+++ consume.py
@@ -2,22 +2,58 @@ from kafka import KafkaConsumer, TopicPartition, SimpleConsumer, SimpleClient
 import json
 import argparse
 
+# Postgres imports
+
+from psycopg2 import connect
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from psycopg2 import OperationalError
+
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--topic',action='store',dest='kafka_topic')
-parser.add_argument('--host',action='store',dest='kafka_host')
-parser.add_argument('--port',action='store',dest='kafka_port')
+parser.add_argument('--topic',action='store',dest='kafka_topic', required=True)
+parser.add_argument('--host',action='store',dest='kafka_host', required=True)
+parser.add_argument('--port',action='store',dest='kafka_port', required=True)
+
+
+parser.add_argument('--db-host',action='store',dest='db_host', required=True)
+# parser.add_argument('--db-port',action='store',dest='db_port', required=True)
+
 
 args = parser.parse_args()
 
 KAFKA_TOPIC = args.kafka_topic
 KAFKA_HOST = args.kafka_host
 KAFKA_PORT = str(args.kafka_port)
+DB_HOST = args.db_host
 
-consumer = KafkaConsumer(bootstrap_servers=":".join([KAFKA_HOST, KAFKA_PORT]), auto_offset_reset='earliest')
+DB_NAME = "crypto_prices"
+
+consumer = KafkaConsumer(bootstrap_servers=":".join([KAFKA_HOST, KAFKA_PORT]), auto_offset_reset='latest')
 
 consumer.assign([TopicPartition(KAFKA_TOPIC, 0)])
+
+# set up Postgres connection and prices database
+try:
+    db_connection = connect(user='postgres',password='example',host=DB_HOST, database=DB_NAME)
+    db_connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+except OperationalError:
+    print("first time record, creating DB...")
+    db_connection = connect(user='postgres',password='example',host=DB_HOST)
+    db_connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    db_cursor = db_connection.cursor()
+    db_cursor.execute("CREATE DATABASE " + DB_NAME)
+    # close cursor and connection and reconnect
+    db_cursor.close()
+    db_connection.close()
+    db_connection = connect(user='postgres',password='example',host=DB_HOST, database=DB_NAME)
+
+
+# set up cursor
+db_cursor = db_connection.cursor()
+
+db_connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+
 
 for message in consumer:
     json_string = message.value.decode('utf8')
--- produce.py
+++ produce.py
@@ -4,27 +4,35 @@ import json
 import argparse
 import requests
 
-parser = argparse.ArgumentParser()
+MSG = '''
 
-parser.add_argument('--topic',action='store',dest='kafka_topic')
-parser.add_argument('--host',action='store',dest='kafka_host')
-parser.add_argument('--port',action='store',dest='kafka_port')
+Produce cryptocurrency prices messages for a Kafka topic. Data is relevant
+for Coinbase-Pro and streamed from Cryptowatch API
+
+Kafka topic is named same as the streamed crypto pair.
+
+For valid crypto pairs visit: https://api.cryptowat.ch/pairs
+
+'''
+
+parser = argparse.ArgumentParser(description=MSG)
+
+# parser.add_argument('--topic',action='store',dest='kafka_topic')
+parser.add_argument('--host',action='store',dest='kafka_host',required=True)
+parser.add_argument('--port',action='store',dest='kafka_port',required=True)
+parser.add_argument('--crypto-pair',action='store',dest='crypto_pair',required=True, default="btcusd")
 
 args = parser.parse_args()
 
-KAFKA_TOPIC = args.kafka_topic
+KAFKA_TOPIC = args.crypto_pair
 KAFKA_HOST = args.kafka_host
 KAFKA_PORT = str(args.kafka_port)
+CRYPTO_PAIR = args.crypto_pair
 
 producer = KafkaProducer(value_serializer=lambda v:json.dumps(v).encode('utf-8'), bootstrap_servers=":".join([KAFKA_HOST, KAFKA_PORT]))
-# producer = KafkaProducer(bootstrap_servers=":".join([KAFKA_HOST, KAFKA_PORT]))
-
-# for _ in range(100):
-#     producer.send(KAFKA_TOPIC, b'some_message_bytes_with sleep')
-#     time.sleep(10)
 
 while True:
-    response = requests.get('https://api.cryptowat.ch/markets/coinbase-pro/btcusd/price')
+    response = requests.get('https://api.cryptowat.ch/markets/coinbase-pro/%s/price' % CRYPTO_PAIR.lower())
     data = response.json()
     producer.send(KAFKA_TOPIC, data)
     time.sleep(10)
