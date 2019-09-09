31feda06c4bb4ec1829d89a4fac30abdf0fef996
vitek <vit.urbanec@gmail.com>
2019-09-09 12:56:31 +0300

    code cleanup

--- kafka_consumer/consume.py
+++ kafka_consumer/consume.py
@@ -15,6 +15,7 @@ import json
 import argparse
 from os import environ
 import time
+import sys
 import logging
 from kafka import KafkaConsumer, TopicPartition
 # from kafka import errors as kafka_errors
@@ -38,19 +39,34 @@ PARSER.add_argument('--envvars', action='store_true')
 
 ARGS = PARSER.parse_args()
 
+# logging setup
+
+FORMATTER = logging.Formatter('%(asctime)-15s %(name)-12s: %(levelname)-8s %(message)s')
+
+LOGGER = logging.getLogger()
+HANDLER = logging.StreamHandler()
+HANDLER.setFormatter(FORMATTER)
+LOGGER.addHandler(HANDLER)
+
+
 if ARGS.envvars:
-    DB_HOST = environ['DB_HOST']
-    DB_PORT = environ['DB_PORT']
-    KAFKA_TOPIC = environ['KAFKA_TOPIC']
-    CRYPTO_PAIR = environ['CRYPTO_PAIR']
-    KAFKA_HOST_PORT = environ['KAFKA_HOST_PORT']
-    KAFKA_SSL_CAFILE = environ['KAFKA_SSL_CAFILE']
-    KAFKA_SSL_CERTFILE = environ['KAFKA_SSL_CERTFILE']
-    KAFKA_SSL_KEYFILE = environ['KAFKA_SSL_KEYFILE']
-    DB_NAME = environ['DB_NAME']
-    DB_USER = environ['DB_USER']
-    DB_PASS = environ['DB_PASS']
-    LOGGING_LEVEL = environ['LOGGING_LEVEL']
+    try:
+        DB_HOST = environ['DB_HOST']
+        DB_PORT = environ['DB_PORT']
+        KAFKA_TOPIC = environ['KAFKA_TOPIC']
+        CRYPTO_PAIR = environ['CRYPTO_PAIR']
+        KAFKA_HOST_PORT = environ['KAFKA_HOST_PORT']
+        KAFKA_SSL_CAFILE = environ['KAFKA_SSL_CAFILE']
+        KAFKA_SSL_CERTFILE = environ['KAFKA_SSL_CERTFILE']
+        KAFKA_SSL_KEYFILE = environ['KAFKA_SSL_KEYFILE']
+        DB_NAME = environ['DB_NAME']
+        DB_USER = environ['DB_USER']
+        DB_PASS = environ['DB_PASS']
+        LOGGING_LEVEL = environ['LOGGING_LEVEL']
+    except KeyError:
+        LOGGER.setLevel(logging.ERROR)
+        LOGGER.error("Incomplete environment variables")
+        sys.exit(1)
 
 else:
 
@@ -59,70 +75,76 @@ else:
     KAFKA_PORT = str(ARGS.kafka_port)
     CRYPTO_PAIR = ARGS.crypto_pair
 
-# logging setup
-
-FORMATTER = logging.Formatter('%(asctime)-15s %(name)-12s: %(levelname)-8s %(message)s')
-
-LOGGER = logging.getLogger()
-HANDLER = logging.StreamHandler()   # by default writes to STDERR when stream is None
-HANDLER.setFormatter(FORMATTER)
-LOGGER.addHandler(HANDLER)
 LOGGING_LEVEL_SETTING = logging.getLevelName(LOGGING_LEVEL)
 LOGGER.setLevel(LOGGING_LEVEL_SETTING)
 
-# Wait for Kafka broker to become available
-for i in range(1, 10):
+
+
+if __name__ == "__main__":
+
+    # Wait for Kafka broker to become available
+
+    for i in range(1, 10):
+        try:
+            LOGGER.info('connecting to Kafka broker\n')
+            consumer = \
+                KafkaConsumer(
+                    auto_offset_reset='latest',
+                    bootstrap_servers=KAFKA_HOST_PORT,
+                    security_protocol="SSL",
+                    ssl_cafile=KAFKA_SSL_CAFILE,
+                    ssl_certfile=KAFKA_SSL_CERTFILE,
+                    ssl_keyfile=KAFKA_SSL_KEYFILE,
+                    )
+            LOGGER.info('Connected to Kafka\n')
+        except Exception as e:
+            LOGGER.info('Cannot connect to Kafka because %s, retrying in 10s', e)
+            time.sleep(10)
     try:
-        LOGGER.info('connecting to Kafka broker\n')
-        consumer = \
-            KafkaConsumer(
-                auto_offset_reset='latest',
-                bootstrap_servers=KAFKA_HOST_PORT,
-                security_protocol="SSL",
-                ssl_cafile=KAFKA_SSL_CAFILE,
-                ssl_certfile=KAFKA_SSL_CERTFILE,
-                ssl_keyfile=KAFKA_SSL_KEYFILE,
-                )
+        consumer.assign([TopicPartition(KAFKA_TOPIC, 0)])
+    except NameError:
+        LOGGER.error('Kafka connection failed')
+        sys.exit(1)
+
+
+    # Set up PostgreSQL connection
+
+    try:
+        LOGGER.info("PostgreSQL connection created")
+        DB_CONNECTION = connect(
+            user=DB_USER,
+            password=DB_PASS,
+            host=DB_HOST,
+            database=DB_NAME,
+            port=DB_PORT
+            )
+        DB_CONNECTION.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        LOGGER.info("PostgreSQL connection created")
     except Exception as e:
-        LOGGER.info('Cannot connect to Kafka broker because %s, retrying in 10s', e)
+        LOGGER.info('Cannot connect to PostgreSQL because %s, retrying in 10s', e)
         time.sleep(10)
 
-consumer.assign([TopicPartition(KAFKA_TOPIC, 0)])
+    # set up cursor
+    DB_CURSOR = DB_CONNECTION.cursor()
 
-DB_CONNECTION = connect(
-    user=DB_USER,
-    password=DB_PASS,
-    host=DB_HOST,
-    database=DB_NAME,
-    port=DB_PORT
-    )
-DB_CONNECTION.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-LOGGER.info("PostgreSQL connection created")
+    DB_CURSOR.execute(
+        'CREATE TABLE IF NOT EXISTS %s (timestamp timestamp WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP, value float)' % KAFKA_TOPIC)
+    DB_CURSOR.execute(
+        'CREATE TABLE IF NOT EXISTS cryptowatch_api_allowance (timestamp timestamp WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP, allowance bigint)')
 
+    for message in consumer:
 
-# set up cursor
-DB_CURSOR = DB_CONNECTION.cursor()
+        json_string = message.value.decode('utf8')
+        try:
+            json_data = json.loads(json_string)
 
-DB_CURSOR.execute(
-    'CREATE TABLE IF NOT EXISTS %s (timestamp timestamp WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP, value float)' % KAFKA_TOPIC)
-DB_CURSOR.execute(
-    'CREATE TABLE IF NOT EXISTS cryptowatch_api_allowance (timestamp timestamp WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP, allowance bigint)')
+            DB_CURSOR.execute('INSERT INTO %s(value) VALUES (%s)' %
+                              (KAFKA_TOPIC, str(json_data[CRYPTO_PAIR]['result']['price'])))
 
-time.sleep(10)
+            DB_CURSOR.execute('INSERT INTO %s(allowance) VALUES (%s)' %
+                              ("cryptowatch_api_allowance",
+                               str(json_data[CRYPTO_PAIR]['allowance']['remaining'])))
 
-for message in consumer:
-
-    json_string = message.value.decode('utf8')
-    try:
-        json_data = json.loads(json_string)
-
-        DB_CURSOR.execute('INSERT INTO %s(value) VALUES (%s)' %
-                          (KAFKA_TOPIC, str(json_data[CRYPTO_PAIR]['result']['price'])))
-
-        DB_CURSOR.execute('INSERT INTO %s(allowance) VALUES (%s)' %
-                          ("cryptowatch_api_allowance",
-                           str(json_data[CRYPTO_PAIR]['allowance']['remaining'])))
-
-        LOGGER.info("Kafka timestamp: %s Record: %s\n" % (str(message.timestamp), str(json_data)))
-    except Exception as e:
-        LOGGER.error(e)
+            LOGGER.info("Kafka timestamp: %s Record: %s\n" % (str(message.timestamp), str(json_data)))
+        except Exception as e:
+            LOGGER.error("Cannot record to PostgreSQL because: %s", e)
